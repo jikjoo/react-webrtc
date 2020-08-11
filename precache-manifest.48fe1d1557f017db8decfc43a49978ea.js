f3ec630d4a2d9c960a2d259ab26e2803cf41e0df
jikjoo <Jikjoo@JIKTOP>
2020-08-11 23:38:47 +0900

    Updates

---
+++ precache-manifest.48fe1d1557f017db8decfc43a49978ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b53c9309cdcf8ca9ec26",
+    "url": "/react-webrtc/static/css/main.b92b3172.chunk.css"
+  },
+  {
+    "revision": "b53c9309cdcf8ca9ec26",
+    "url": "/react-webrtc/static/js/main.c08fc37e.chunk.js"
+  },
+  {
+    "revision": "cdac2779de9ee86686c8",
+    "url": "/react-webrtc/static/js/runtime~main.7a01059f.js"
+  },
+  {
+    "revision": "35adef7ffaa16240f2c4",
+    "url": "/react-webrtc/static/js/2.09ce1b3c.chunk.js"
+  },
+  {
+    "revision": "63622322aa6367496342307ac9fc6cc7",
+    "url": "/react-webrtc/index.html"
+  }
+];
