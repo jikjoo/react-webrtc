be2106a962b4909554f8f11d67bc52dcb7486d2f
jikjoo <Jikjoo@JIKTOP>
2020-08-23 19:09:23 +0900

    Updates

---
+++ precache-manifest.9855c91496a4a0deca56dbc4ad4b0f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3919a5f411065969e9ba",
+    "url": "/react-webrtc/static/css/main.b92b3172.chunk.css"
+  },
+  {
+    "revision": "3919a5f411065969e9ba",
+    "url": "/react-webrtc/static/js/main.ffc33713.chunk.js"
+  },
+  {
+    "revision": "cdac2779de9ee86686c8",
+    "url": "/react-webrtc/static/js/runtime~main.7a01059f.js"
+  },
+  {
+    "revision": "be9398f541d81fafd6b3",
+    "url": "/react-webrtc/static/js/2.fb75d1b7.chunk.js"
+  },
+  {
+    "revision": "34439244953ea3d60b9b1f77b4f1e8e4",
+    "url": "/react-webrtc/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.069a867c23466467d55d05300ed7d546.js"
+  "/react-webrtc/precache-manifest.9855c91496a4a0deca56dbc4ad4b0f34.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-webrtc/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
